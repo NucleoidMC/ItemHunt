8db08a1aa6f36432bcbb2c5217846d5aa6af0f3e
Jerozgen <jerozgen@gmail.com>
2023-06-09 21:03:44 +0300

    Show raw name in GUI

--- src/main/java/io/github/jerozgen/itemhunt/game/ItemHuntUtils.java
+++ src/main/java/io/github/jerozgen/itemhunt/game/ItemHuntUtils.java
@@ -9,13 +9,14 @@ import java.util.UUID;
 
 public class ItemHuntUtils {
     public static String getEncodedSkinTexture(String skinUrlHash) {
-        var value = "{\"textures\":{\"SKIN\":{\"url\":\"http://textures.minecraft.net/texture/%s\"}}}".formatted(skinUrlHash);
+        var value = """
+                {"textures":{"SKIN":{"url":"http://textures.minecraft.net/texture/%s"}}}""".formatted(skinUrlHash);
         return new String(Base64.getEncoder().encode(value.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
     }
 
     public static Text getPlayerNameByUUID(UUID uuid, MinecraftServer server) {
         var player = server.getPlayerManager().getPlayer(uuid);
-        if (player != null) return player.getDisplayName();
+        if (player != null) return player.getName();
 
         var profile = server.getUserCache().getByUuid(uuid);
         return Text.of(profile.isEmpty() ? uuid.toString() : profile.get().getName());
